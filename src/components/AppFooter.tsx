451490479ca2df975bc8995d79b2984e931d026e
sunya9 <mugen.xyz@gmail.com>
2020-11-10 04:00:49 +0900

    feat: add icon

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,3 +1,28 @@
+import { GitHub, Twitter } from "react-feather";
+
 export const AppFooter = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <hr />
+      <div className="center">
+        <p>
+          <a href="https://twitter.com/">
+            <Twitter size="1.2rem" />
+          </a>
+          &nbsp;&nbsp;
+          <a href="https://twitter.com/sunya9">
+            <GitHub size="1.2rem" />
+          </a>
+        </p>
+        <p>Created by _X_y_z_.</p>
+      </div>
+      <style jsx>
+        {`
+          .center {
+            text-align: center;
+          }
+        `}
+      </style>
+    </footer>
+  );
 };
