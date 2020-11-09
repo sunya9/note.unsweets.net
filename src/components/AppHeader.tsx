39180f5dd209ca0e9bfbd1c4f8e9357cba1099ba
sunya9 <mugen.xyz@gmail.com>
2020-11-10 02:57:24 +0900

    feat: surround title with anchor if not index page

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,22 @@
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
 import { config } from "../../blog.config";
 
+const Title = () => {
+  const router = useRouter();
+  if (router.pathname === "/") return <>{config.title()}</>;
+  return (
+    <Link href="/">
+      <a>{config.title()}</a>
+    </Link>
+  );
+};
 export const AppHeader = () => {
   return (
     <header>
-      <h1>{config.title()}</h1>
+      <h1>
+        <Title />
+      </h1>
       <p>{config.description}</p>
     </header>
   );
