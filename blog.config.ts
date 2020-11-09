065974373502bb22a2a71f328bcd36984c953404
sunya9 <mugen.xyz@gmail.com>
2020-11-10 05:41:32 +0900

    refactor: move some values to config

--- blog.config.ts
+++ blog.config.ts
@@ -1,10 +1,18 @@
-const title = "";
-const description = "";
+import appRootPath from "app-root-path";
+import * as path from "path";
+const rootPath = appRootPath.toString();
+
+const title = "あああ";
+const description = "web開発やらAndroidに関するノート。";
 const blogDir = "notes";
+const contentsPath = path.resolve(rootPath, "contents");
+const noteDirPath = path.resolve(contentsPath, blogDir);
 
 export const config = {
   title: (pageTitle?: string) =>
     pageTitle ? `${pageTitle} - ${title}` : title,
   description,
   blogDir,
+  noteDirPath,
+  contentsPath,
 } as const;
